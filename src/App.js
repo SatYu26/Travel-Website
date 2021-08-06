020611eca7fa3a434832158b90170c1da4128f7b
SatYu26 <goyalsatyam8@gmail.com>
2021-08-06 21:09:11 +0530

    Files Refractoring, Creating Cards Componet and Footer Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 import "./App.css";
-import NavBar from "./components/NavBar/NavBar.js";
+import Home from "./components/pages/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./components/Pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
     <>
       <Router>
-        <NavBar />
+        <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
     </>
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,11 +1,15 @@
-import React from "react";
-import "../../App.css";
-import HeroSection from "../HeroSection/HeroSection";
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
+      <Footer />
     </>
   );
 }
