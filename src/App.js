a2735764e9d798fa4d8af337a0cb49f49821d04e
SatYu26 <goyalsatyam8@gmail.com>
2021-08-06 20:47:56 +0530

    creating a hero component and home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Pages/Home";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <NavBar />
         <Switch>
-          <Route path="/" exact />
+          <Route path="/" exact component={Home} />
         </Switch>
       </Router>
     </>
